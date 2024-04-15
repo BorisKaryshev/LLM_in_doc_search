abd7d67756f1c4bb91568ae561ff9439545f340f
BorisKaryshev <boris.karyshev@gmail.com>
2024-04-15 10:42:25 +0300

    Fixed venv creation command

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 echo "Started"
 
 path_to_venv='./venv'
-python -m venv "$path_to_venv"
+python3 -m pip install --user virtualenv
+python3 -m venv "$path_to_venv"
 
-"./$path_to_venv/activate*"
-python -m pip install -r ./requirements.txt
+"./$path_to_venv/bin/activate"
+python3 -m pip install -r ./requirements.txt
 
 echo "Finished"
