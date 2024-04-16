5b4a1a2cd095d9fc3f2472fcd667f78757aba282
BorisKaryshev <boris.karyshev@gmail.com>
2024-04-16 10:09:58 +0300

    Added run command

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ python3 -m venv "$path_to_venv"
 source "./$path_to_venv/bin/activate"
 python3 -m pip install -r ./requirements.txt
 
-echo "Finished"
+echo "Finished, running"
+
+python3 main.py --config datasheets.json --searcher one_doc
